ddd29ba8e7c26ecf6f535c2fde72f1cba0211af7
guadasandoval <guadastoop@gmail.com>
2023-12-20 17:31:46 -0300

    rediseno

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './assets/css/App.css';
 import { useState } from 'react';
 import Card from './Card'
+import oraculo from './assets/static/oraculo.png'
 
 function App() {
   const [showComponent, setShowComponent] = useState(true);
@@ -17,20 +18,13 @@ function App() {
   
   return (
     <div className="App">
-      <div className='div-container' id="star-1" style={{top: 100,
-    left: 370}}>
-        <svg data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 246 247">
-        <polygon className="cls-1" points="195.28 198.84 143.47 172.43 122.48 226.66 104.52 171.35 51.33 194.86 77.74 143.05 23.5 122.06 78.81 104.09 55.31 50.9 107.12 77.31 128.11 23.08 146.07 78.38 199.26 54.88 172.86 106.69 227.09 127.68 171.78 145.65 195.28 198.84"/>
-      </svg>
-      </div>
-      <div id="star-2" className='div-container' style={{top: 30, right: 290}}>
-        <svg  data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 246 247">
-        <polygon className="cls-1" points="195.28 198.84 143.47 172.43 122.48 226.66 104.52 171.35 51.33 194.86 77.74 143.05 23.5 122.06 78.81 104.09 55.31 50.9 107.12 77.31 128.11 23.08 146.07 78.38 199.26 54.88 172.86 106.69 227.09 127.68 171.78 145.65 195.28 198.84"/>
-      </svg>
-      </div>
       <div className='info-container'>
-      {showComponent ? (
-        <h1>Hacé una pregunta al oráculo</h1>
+        <div className='border'>              
+        {showComponent ? (
+        <img className='img-app' src={oraculo} alt=''/>
+      ) : null}
+        {showComponent ? (
+        <h1>Preguntá al oráculo</h1>
       ) : null}
         {showComponent ? (
         <div>
@@ -41,21 +35,10 @@ function App() {
       {!showComponent && showPreviousComponent && <Card/>}
       {showPreviousComponent ? (
         <div>
-          <button onClick={handleBackButtonClick}>Volver a preguntar</button>
+          <button onClick={handleBackButtonClick}>Preguntar de nuevo</button>
         </div>
       ) : null}
-     
-      </div>
-      <div id="star-3" className='div-container' style={{top: 550,
-    left: 170}}>
-        <svg  data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 246 247">
-        <polygon className="cls-1" points="195.28 198.84 143.47 172.43 122.48 226.66 104.52 171.35 51.33 194.86 77.74 143.05 23.5 122.06 78.81 104.09 55.31 50.9 107.12 77.31 128.11 23.08 146.07 78.38 199.26 54.88 172.86 106.69 227.09 127.68 171.78 145.65 195.28 198.84"/>
-      </svg>
-      </div>
-      <div id="star-4" className='div-container' style={{top: 600, right: 100}}>
-        <svg data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 246 247">
-        <polygon className="cls-1" points="195.28 198.84 143.47 172.43 122.48 226.66 104.52 171.35 51.33 194.86 77.74 143.05 23.5 122.06 78.81 104.09 55.31 50.9 107.12 77.31 128.11 23.08 146.07 78.38 199.26 54.88 172.86 106.69 227.09 127.68 171.78 145.65 195.28 198.84"/>
-      </svg>
+        </div>
       </div>
       <p>@GUADA.SANDOVAL©2023</p>
     </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import './assets/css/Card.css'
 import confia from './assets/static/confia.png'
+import partir from './assets/static/partir.png'
+import calma from './assets/static/calma.png'
+import paciencia from './assets/static/paciencia.png'
+import tiempo from './assets/static/tiempo.png'
 
 export default function Card() {
   const [selectedItem, setSelectedItem] = useState(null);
@@ -13,15 +17,15 @@ export default function Card() {
 
   useEffect(() => { 
     let items = [
-        {img: '', quote: 'Es momento de partir'},
+        {img: partir, quote: 'Es momento de partir'},
         {img: '', quote: 'No vale la pena'},
-        {img: '', quote: 'Tomalo con calma'},
-        {img: '', quote: 'Confiá en vos'},
+        {img: calma, quote: 'Tomalo con calma'},
+        {img: confia, quote: 'Confiá en vos'},
         {img: '', quote: 'Falta algo'},
         {img: '', quote: 'Tomá acción'},
-        {img: '', quote: 'Paciencia'},
+        {img: paciencia, quote: 'Paciencia'},
         {img: '', quote: 'Está cerca'},
-        {img: '', quote: 'En un tiempo'},
+        {img: tiempo, quote: 'En un tiempo'},
         {img: '', quote: 'Claramente, SI'},
     ]
     const timeoutId = setTimeout(() => {
@@ -37,11 +41,11 @@ export default function Card() {
     <div className="card">
         {selectedItem ? (
         <div>
-          <img src={confia} alt="" className='img-card'/>
-          <h1 className='h1-card'>{selectedItem.quote}</h1>
+          <img src={selectedItem.img} alt="" className='img-card'/>
+          <h2 className='h2-card'>{selectedItem.quote}</h2>
         </div>
       ) : (
-        <p>Seleccionando...</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="#f1ab1e"  strokeLinejoin="round" width="30" height="30" x="85" y="85" rx="0" ry="0"><animate attributeName="rx" calcMode="spline" dur="0.7" values="15;15;5;15;15" keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate><animate attributeName="ry" calcMode="spline" dur="0.7" values="15;15;10;15;15" keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate><animate attributeName="height" calcMode="spline" dur="0.7" values="30;30;1;30;30" keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate><animate attributeName="y" calcMode="spline" dur="0.7" values="40;170;40;" keySplines=".6 0 1 .4;0 .8 .2 1" repeatCount="indefinite"></animate></rect></svg>
       )}
     </div>
    
